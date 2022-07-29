4ee5582d784fb1115711b33fb035dc872c7f2497
alexismarroquin7 <alexismarroquin754@gmail.com>
2022-07-28 23:57:41 -0700

    removes unused code

--- store/slices/index.js
+++ store/slices/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { quoteSlice } from "./quote-slice";
-import { verizonSlice } from "./verizon-slice";
 
 export const rootReducer = combineReducers({
-  verizon: verizonSlice.reducer,
   quote: quoteSlice.reducer
 })
