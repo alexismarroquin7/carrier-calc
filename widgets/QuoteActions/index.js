38c5a9930c79a90c224b9b4285f537b6a0fb13eb
alexismarroquin7 <alexismarroquin754@gmail.com>
2022-08-03 16:25:31 -0700

    due today color changed to green outline

--- widgets/QuoteActions/index.js
+++ widgets/QuoteActions/index.js
@@ -11,241 +11,196 @@ const todaysDate = () => {
   let month = d.getMonth() + 1;
   let date = d.getDate();
   let year = d.getFullYear();
+  let hr = d.getHours();
+  let min = d.getMinutes();
+  let sec = d.getSeconds();
+
+
   if(month<10){
     month = `0${month}`;
   }
   if(date<10){
     date = `0${date}`;
   }
+  if(hr<10){
+    hr = `0${hr}`;
+  }
+  if(min<10){
+    min = `0${min}`;
+  }
+  if(sec<10){
+    sec = `0${sec}`;
+  }
 
-  today = `${month}-${date}-${year}`;
+  today = `${month}-${date}-${year}T${hr}-${min}-${sec}`;
 
   return today;
 }
 
-const generateQuoteDocument = (type, quote) => {
+const generateQuoteDocument = (quote) => {
+  let linesHtml = `<table>
+  <tr>
+    <th>Line</th>
+    <th>Type</th>
+    <th>Name</th>
+    <th>Phone Number</th>
 
-  switch(type){
-    case 'text/plain':
-      let linesPlainText = '';
+    <th>Device</th>
+    <th>Price</th>
+    <th>Downpayment</th>
+    <th>Trade In Credit</th>
+    <th>(Device) Due Today</th>
+    <th>(Device) Due Monthly</th>
 
-      quote.lines.map((line, i) => {
-        linesPlainText += `------------------------
-  Line: ${i+1}
-    Type: ${line.type}    
-    Name: ${line.name}    
-    Phone Number: ${line.phoneNumber}
-    ------------------------
-    Device:
-      Name: ${line.device.name} 
-      Price: $${line.device.price ? Number(line.device.price).toFixed(2) : 0.00} 
-      Downpayment: $${line.device.downpayment ? Number(line.device.downpayment).toFixed(2) : 0.00}    
-      Trade In Credit: $${line.device.tradeInCredit ? Number(line.device.tradeInCredit).toFixed(2) : 0.00}    
-      Due:
-        Today: $${line.device.dueToday ? Number(line.device.dueToday).toFixed(2) : 0.00}    
-        Monthly: $${line.device.dueMonthly ? Number(line.device.dueMonthly).toFixed(2) : 0.00}
-    ------------------------
-    Plan:
-      Name: ${line.plan.name ? line.plan.name : 'None'} 
-      Due:
-        Monthly: $${line.plan.dueMonthly ? Number(line.plan.dueMonthly).toFixed(2) : 0.00} 
-    ------------------------
-    Protection:
-      Name: ${line.protection.name ? line.protection.name : 'None'} 
-      Due:
-        Today: $${line.protection.dueToday ? Number(line.protection.dueToday).toFixed(2) : 0.00} 
-        Monthly: $${line.protection.dueMonthly ? Number(line.protection.dueMonthly).toFixed(2) : 0.00} 
-`
-      })
-      
-      return `
-${todaysDate()}
-${quote.name}
-------------------------
-Carrier: ${quote.carrier.title}
-------------------------
-Due:
-  Today: $${calcQuoteDueToday(quote).toFixed(2)}
-  Monthly: $${calcQuoteDueMonthly(quote).toFixed(2)}
-------------------------
-Account:
-  Plan: 
-    Name: ${quote.account.plan.name ? quote.account.plan.name : "None"}
-    Due:
-      Monthly: $${Number(quote.account.plan.dueMonthly).toFixed(2)}
-  Protection:
-    Name: ${quote.account.protection.name ? quote.account.protection.name : "None"}
-    Due:
-      Monthly: $${Number(quote.account.protection.dueMonthly).toFixed(2)}
-------------------------
-Lines: ${quote.lines.length}
-  ${linesPlainText}
-`
+    <th>Plan</th>
+    <th>(Plan) Due Monthly</th>
+    <th>Protection</th>
+    <th>(Protection) Due Today</th>
+    <th>(Protection) Due Monthly</th>
     
-    case 'text/html':
-      let linesHtml = `<table>
-      <tr>
-        <th>Line</th>
-        <th>Type</th>
-        <th>Name</th>
-        <th>Phone Number</th>
-
-        <th>Device</th>
-        <th>Price</th>
-        <th>Downpayment</th>
-        <th>Trade In Credit</th>
-        <th>(Device) Due Today</th>
-        <th>(Device) Due Monthly</th>
-
-        <th>Plan</th>
-        <th>(Plan) Due Monthly</th>
-        <th>Protection</th>
-        <th>(Protection) Due Today</th>
-        <th>(Protection) Due Monthly</th>
-        
-      </tr>
-    `;
-      
-      quote.lines.forEach((line, i) => {
-        linesHtml += `
-        <tr>
-          <td>${i+1}</td>
-          <td>${line.type}</td>
-          <td>${line.name}</td>
-          <td>${line.phoneNumber}</td>
-          <td>${line.device.name}</td>
-          <td>$${Number(line.device.price).toFixed(2)}</td>
-          <td>$${Number(line.device.downpayment).toFixed(2)}</td>
-          <td>$${Number(line.device.tradeInCredit).toFixed(2)}</td>
-          <td>$${Number(line.device.dueToday).toFixed(2)}</td>
-          <td>$${Number(line.device.dueMonthly).toFixed(2)}</td>
-          <td>${line.plan.name ? line.plan.name : 'None'}</td>
-          <td>$${Number(line.plan.dueMonthly).toFixed(2)}</td>
-          <td>${line.protection.name ? line.protection.name : 'None'}</td>
-          <td>$${Number(line.protection.dueToday).toFixed(2)}</td>
-          <td>$${Number(line.protection.dueMonthly).toFixed(2)}</td>
-        </tr>
-        `
-      })
-
-      linesHtml += `</table>`
-
-      return `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>${quote.name} ${quote.carrier.title} ${quote.lines.length} ${quote.lines.length === 1 ? 'Line' : 'Lines'}</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        </head>
-      
-        <body>
-          <div class="wrapper">
-            <h1>Member Quote Comparison</h1>
-            <h2>${quote.name} | ${quote.carrier.title} | ${quote.lines.length} ${quote.lines.length === 1 ? 'Line' : 'Lines'}</h2>
-            <h3>${todaysDate()}</h3>
-            
-            <div class="due-wrapper">
-              <h2 class="due">Due Today:
-                <span>$${calcQuoteDueToday(quote).toFixed(2)}</span>
-              </h2>
-              <h2 class="due">Due Monthly: 
-                <span>$${calcQuoteDueMonthly(quote).toFixed(2)}</span>
-              </h2>
-            </div>
-
-            <div class="account-wrapper">
-              <h3>Account</h3>
-              <h4 class="account-feature">Plan: 
-                ${quote.account.plan.name ? quote.account.plan.name : 'None'}
-                <span>Due Monthly: $${Number(quote.account.plan.dueMonthly).toFixed(2)}</span>
-              </h4>
-
-              <h4 class="account-feature">Protection: 
-                ${quote.account.protection.name ? quote.account.protection.name : 'None'}
-                <span>Due Monthly: $${Number(quote.account.protection.dueMonthly).toFixed(2)}</span>
-              </h4>
-            </div>
-
-            <div class="lines">
-              <h3>Lines: ${quote.lines.length}</h3>
-              ${linesHtml}
-            </div>
-          </div>
-        </body>
-          
-        <style>
-          @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-
-          * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            font-family: 'Roboto', sans-serif;
-          }
-
-          body {
-            width: 100%;
-            display: flex;
-            flex-flow: column wrap;
-            align-items: center;
-          }
-
-          .wrapper {
-            width: 100%;
-            display: flex;
-            flex-flow: column wrap;
-            align-items: flex-start;
-          }
-
-          .due-wrapper {
-            width: 100%;
-            display: flex;
-            flex-flow: row wrap;
-            justify-content: center;
-            gap: 1rem;
-          }
-
-          .due {
-            display: flex;
-            flex-flow: column wrap;
-            align-items: center;
-          }
-
-          .account-wrapper {
-            display: flex;
-            flex-flow: column wrap;
-          }
-
-          .account-feature {
-            border: 1px solid black;
-            padding: 1rem;
-            display: flex;
-            flex-flow: column wrap;
-            align-items: flex-start;
-            gap: 1rem;
-          }
-
-          table {
-            border: 1px solid black;
-            padding: 1rem;
-            border-collapse: collapse;
-          }
-
-          th {
-            border: 1px solid black;
-          }
-
-          td {
-            border: 1px solid black;
-            padding: 1rem;
-          }
-
-        </style>
-      </html>
+  </tr>
+  `;
+  
+  quote.lines.forEach((line, i) => {
+    linesHtml += `
+    <tr>
+      <td>${i+1}</td>
+      <td>${line.type}</td>
+      <td>${line.name}</td>
+      <td>${line.phoneNumber}</td>
+      <td>${line.device.name}</td>
+      <td>$${Number(line.device.price).toFixed(2)}</td>
+      <td>$${Number(line.device.downpayment).toFixed(2)}</td>
+      <td>$${Number(line.device.tradeInCredit).toFixed(2)}</td>
+      <td>$${Number(line.device.dueToday).toFixed(2)}</td>
+      <td>$${Number(line.device.dueMonthly).toFixed(2)}</td>
+      <td>${line.plan.name ? line.plan.name : 'None'}</td>
+      <td>$${Number(line.plan.dueMonthly).toFixed(2)}</td>
+      <td>${line.protection.name ? line.protection.name : 'None'}</td>
+      <td>$${Number(line.protection.dueToday).toFixed(2)}</td>
+      <td>$${Number(line.protection.dueMonthly).toFixed(2)}</td>
+    </tr>
     `
-    default:
-      throw Error(`unkown document type: ${type}`);
-  }
+  });
+
+  linesHtml += `</table>`;
+
+  const html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>${quote.name} ${quote.carrier.title} ${quote.lines.length} ${quote.lines.length === 1 ? 'Line' : 'Lines'}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+  
+    <body>
+      <div class="wrapper">
+        <h1>Member Quote</h1>
+        <h2>${quote.name ? quote.name : 'Untitled'} | ${quote.carrier.title} | ${quote.lines.length} ${quote.lines.length === 1 ? 'Line' : 'Lines'}</h2>
+        <h3>${todaysDate()}</h3>
+        
+        <div class="due-wrapper">
+          <h2 class="due">Due Today:
+            <span>$${calcQuoteDueToday(quote).toFixed(2)}</span>
+          </h2>
+          <h2 class="due">Due Monthly: 
+            <span>$${calcQuoteDueMonthly(quote).toFixed(2)}</span>
+          </h2>
+        </div>
+
+        <div class="account-wrapper">
+          <h3>Account</h3>
+          <h4 class="account-feature">Plan: 
+            ${quote.account.plan.name ? quote.account.plan.name : 'None'}
+            <span>Due Monthly: $${Number(quote.account.plan.dueMonthly).toFixed(2)}</span>
+          </h4>
+
+          <h4 class="account-feature">Protection: 
+            ${quote.account.protection.name ? quote.account.protection.name : 'None'}
+            <span>Due Monthly: $${Number(quote.account.protection.dueMonthly).toFixed(2)}</span>
+          </h4>
+        </div>
+
+        <div class="lines">
+          <h3>Lines: ${quote.lines.length}</h3>
+          ${linesHtml}
+        </div>
+      </div>
+    </body>
+      
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+      }
+
+      body {
+        width: 100%;
+        display: flex;
+        flex-flow: column wrap;
+        align-items: center;
+      }
+
+      .wrapper {
+        width: 100%;
+        display: flex;
+        flex-flow: column wrap;
+        align-items: flex-start;
+      }
+
+      .due-wrapper {
+        width: 100%;
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: center;
+        gap: 1rem;
+      }
+
+      .due {
+        display: flex;
+        flex-flow: column wrap;
+        align-items: center;
+      }
+
+      .account-wrapper {
+        display: flex;
+        flex-flow: column wrap;
+      }
+
+      .account-feature {
+        border: 1px solid black;
+        padding: 1rem;
+        display: flex;
+        flex-flow: column wrap;
+        align-items: flex-start;
+        gap: 1rem;
+      }
+
+      table {
+        border: 1px solid black;
+        padding: 1rem;
+        border-collapse: collapse;
+      }
+
+      th {
+        border: 1px solid black;
+      }
+
+      td {
+        border: 1px solid black;
+        padding: 1rem;
+      }
+
+    </style>
+  </html>
+`
+
+  return html;
+
 }
 
 export const QuoteActions = () => {
@@ -332,6 +287,7 @@ export const QuoteActions = () => {
               onClick={(e) => {
                 e.preventDefault();
                 toggleCarrierModalOpen();
+                toggleMenuActive();
               }}
             >{'Cancel'}</button>
           </div>
@@ -342,7 +298,7 @@ export const QuoteActions = () => {
           onClick={(e) => {
             e.preventDefault();
             dispatch(quoteSlice.actions.duplicateSelectedQuote());
-            toggle();
+            toggleMenuActive();
           }}    
           disabled={list.length === 0}
         >Duplicate</button>
@@ -351,11 +307,11 @@ export const QuoteActions = () => {
           onClick={(e) => {
             e.preventDefault();
             const element = document.createElement("a");
-            const file = new Blob([generateQuoteDocument('text/html', quote)], {
+            const file = new Blob([generateQuoteDocument(quote)], {
               type: "text/html"
             });
             element.href = URL.createObjectURL(file);
-            element.download = `${todaysDate()}-${quote.name}-${quote.carrier.name}-${quote.lines.length}-lines.html`;
+            element.download = `${todaysDate()}-${quote.name ? quote.name : 'Untitled'}-${quote.carrier.name ? quote.carrier.name : 'No Carrier Selected'}-${quote.lines.length}-lines.html`;
             document.body.appendChild(element);
             element.click();
           }}   
@@ -369,6 +325,7 @@ export const QuoteActions = () => {
               e.preventDefault();
               dispatch(quoteSlice.actions.deleteSelectedQuote());
               toggleDeleteModeActive();
+              toggleMenuActive();
             }}    
           >Confirm Delete</button>
           <button
